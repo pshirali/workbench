b4dc40558be1dbbc5194f6e7d01dc3de8224d179
Praveen G Shirali <praveengshirali@gmail.com>
2026-04-18 06:51:07 +0530

    Bumped version to 0.1.1 in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Praveen G Shirali'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
